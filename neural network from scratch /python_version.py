048ec4e7de64f5c67aadf2ec6e30079f8df2a786
Graf Stor <48201603+grafstor@users.noreply.github.com>
2020-06-25 13:06:54 +0300

    rename

--- neural network from scratch /python_version.py
+++ neural network from scratch /python_version.py
@@ -9,19 +9,17 @@ __version__ = "1.0"
 
 E = 2.71828182846
 
-def dot(a, b):
-    main = []
+def dot(a, b): 
+    first = []
     for arr in a:
-        time = []
+        second = []
         for i in range(len(b[0])):
-            plus = 0
+            summary = 0
             for p in range(len(arr)):
-                plus+=arr[p]*b[p][i]
-            time.append(plus)
-        main.append(time)
-    return main
-
-make_weights = lambda shape: [[0 for j in range(shape[1])] for i in range(shape[0])]
+                summary+=arr[p]*b[p][i]
+            second.append(summary)
+        first.append(second)
+    return first
 
 sigmoid = lambda x: [1 / (1 + E**(-k[0])) for k in x]
 
@@ -30,7 +28,7 @@ sigmoid_derivative = lambda x: [k * (1 - k) for k in x]
 T = lambda a: [[a[j][i] for j in range(len(a))] for i in range(len(a[0]))]
 
 def main():
-    weights = make_weights((3, 1))
+    weights = [[0.23],[-0.46],[0.53]]
 
     train_x = [[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]]
     train_y = [0, 1, 1, 0]
@@ -41,13 +39,13 @@ def main():
         error = [y-p for y, p in zip(train_y, output)]
 
         delta = [[e*d] for e, d in zip(error, sigmoid_derivative(output))]
-
         adjustment = dot(T(train_x), delta)
+
 
         for i in range(len(weights)):
             weights[i][0] += adjustment[i][0]
-
     print(sigmoid(dot([[1, 0, 1]], weights)))
 
 if __name__ == '__main__':
     main()
+
