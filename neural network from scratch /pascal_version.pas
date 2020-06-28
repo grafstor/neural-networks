4ab2638937c59869dabe081eca9464355d8755f3
Graf Stor <48201603+grafstor@users.noreply.github.com>
2020-06-28 19:57:55 +0300

    version 2.0

--- neural network from scratch /pascal_version.pas
+++ neural network from scratch /pascal_version.pas
@@ -5,140 +5,86 @@
 (*    date: 27.06.20*)
 (*/////////////////////////*)
 
-(*verison 1.0*)
+(*verison 2.0*)
 
-program Hello;
-uses Crt;
+program NN;
 
 var 
-    train_x: array [1..12] of integer;
-    train_y: array [1..4] of real;
-    weigths: array [1..4] of real;
+    train_x: array [0..11] of integer = (0,0,1,  1,1,1,  1,0,1,  0,1,1);
+    train_y: array [0..3] of real = (0,1,1,0);
+    weigths: array [0..2] of real = (-0.145, -0.993, -0.359);
+    
     dot_product: array [1..4] of real;
     output: array [1..4] of real;
+    
     error: array [1..4] of real;
     sigmoid_derivative: array [1..4] of real;
     delta: array [1..4] of real;
     adjustment: array [1..3] of real;
-    train_x_T: array [1..12] of integer;
+    train_x_reversed: array [0..11] of integer = (0,1,1,0, 0,1,0,1, 1,1,1,1);
     
-    test: array [1..3] of real;
+    test: array [0..2] of real = (1,0,1);
     
     summary, result: real;
     
-    i, j, p, epoch: integer;
+    i, p, epoch: integer;
+    
+function sigmoid(x:real): real;
+begin
+    sigmoid := 1 / (1 + exp(-x*LN(2.71828182846)));
+end;
+
+function dsigmoid(x:real): real;
+begin
+    dsigmoid := x*(1 - x);
+end;
 
 begin
-
-    weigths[0] := -0.145;
-    weigths[1] := -0.993;
-    weigths[2] := -0.359;
-
-    train_y[0] := 0;
-    train_y[1] := 1;
-    train_y[2] := 1;
-    train_y[3] := 0;
     
-    
-    train_x[0] := 0;
-    train_x[1] := 0;
-    train_x[2] := 1;
-    
-    train_x[3] := 1;
-    train_x[4] := 1;
-    train_x[5] := 1;
-    
-    train_x[6] := 1;
-    train_x[7] := 0;
-    train_x[8] := 1;
-    
-    train_x[9] := 0;
-    train_x[10] := 1;
-    train_x[11] := 1;
-
-
-    train_x_T[0] := 0;
-    train_x_T[1] := 1;
-    train_x_T[2] := 1;
-    train_x_T[3] := 0;
-    
-    train_x_T[4] := 0;
-    train_x_T[5] := 1;
-    train_x_T[6] := 0;
-    train_x_T[7] := 1;
-    
-    train_x_T[8] := 1;
-    train_x_T[9] := 1;
-    train_x_T[10] := 1;
-    train_x_T[11] := 1;
-
-    
-    for  epoch := 0 to 1000 do
+    for  epoch := 0 to 1200 do
     begin
         
         for  i := 0 to 3 do
         begin
             summary := 0;
-        
             for p := 0 to 2 do
                 summary := summary + train_x[i*3+p] * weigths[p];
-
             dot_product[i] := summary;
         end;
         
         for i := 0 to 3 do
-            output[i] := 1 / (1 + exp(-dot_product[i]*LN(2.71828182846)));
+            output[i] := sigmoid(dot_product[i]);
 
-        
-        
         for i := 0 to 3 do
             error[i] := train_y[i] - output[i];
-        
-    
     
         for i := 0 to 3 do
-            sigmoid_derivative[i] := output[i] * (1 - output[i]);
-
+            sigmoid_derivative[i] := dsigmoid(output[i]);
         
         for i := 0 to 3 do
             delta[i] := error[i] * sigmoid_derivative[i];
 
-        
-    
         for  i := 0 to 2 do
         begin
             summary := 0;
-        
             for p := 0 to 3 do
-                summary := summary + train_x_T[i*4+p] * delta[p];
-
+                summary := summary + train_x_reversed[i*4+p] * delta[p];
             adjustment[i] := summary;
         end;
-        
         
         for i := 0 to 2 do
             weigths[i] := weigths[i] + adjustment[i];
     end;
     
-
-    test[0] := 1;
-    test[1] := 0;
-    test[2] := 1;
-    
-    (* test predict *)
     summary := 0;
-        
     for p := 0 to 2 do
         summary := summary + test[p] * weigths[p];
-    
-    result := 1 / (1 + exp(-summary*LN(2.71828182846)));
+    result := sigmoid(summary);
 
     write('test result: ');
     writeln(result);
-    
     
     writeln('weigths: ');
     for i := 0 to 2 do
         writeln(weigths[i]);
 end.
-
